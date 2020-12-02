d81be1dc4f9b89b24f2b6dbda68bccc0aacffc8e
Justin Reich <justinreich.dev@gmail.com>
2020-12-02 11:02:34 -0600

    feat: Add exercise solution to HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,88 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Test");
+//        // ============================ SYNTAX, TYPES, VARIABLES EXERCISE
+//
+//        // Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+//             int myFavoriteNumber = 7;
+//             System.out.println(myFavoriteNumber);
+//
+//        // Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+//             String myString = "Hello";
+//             System.out.println(myString);
+//
+//        // Change your code to assign a character value to myString. What do you notice?
+//             myString = 'h'; // ERROR
+//             System.out.println(myString);
+//
+//        // Change your code to assign the value 3.14159 to myString. What happens?
+//             myString = 3.14159; // ERROR
+//
+//        // Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+//              long myNumber;
+
+//        // Change your code to assign the value 3.14 to myNumber. What do you notice?
+//             myNumber = 3.14; // ERROR
+//
+//        // Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+//             myNumber = 123L;
+//
+//        // Change your code to assign the value 123 to myNumber.
+//             myNumber = 123;
+//
+//        // Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+////             Longs can only hold integers
+//
+//        // Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+//             float myNumber = 3.14F; // ERROR
+//            // use F after the number or explicit casting
+//
+//
+//        // Copy and paste the following code blocks one at a time and execute them
+
+//             int x = 5;
+//             System.out.println(x++);
+//             System.out.println(x);
+//
+//             int x = 5;
+//             System.out.println(++x);
+//             System.out.println(x);
+//
+//
+//        // What is the difference between the above code blocks? Explain why the code outputs what it does.
+//
+////             the difference between pre and post increment
+//
+//        // Try to create a variable named class. What happens?
+//
+//            // ERROR unexpected identifier
+
+//        Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
+
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+        // error caught at runtime
+
+//        How is the above example different from the code block below?
+//            int three = (int) "three"; // error caught during compilation
+//
+//        // Rewrite the following expressions using the relevant shorthand assignment operators:
+//
+//             int x = 4;
+//             x += 5;
+//
+//             int x = 3;
+//             int y = 4;
+//             y *= x;
+//
+//             int x = 10;
+//             int y = 2;
+//             x /= y;
+//             y -= x;
+
+
+
     }
 
 }
