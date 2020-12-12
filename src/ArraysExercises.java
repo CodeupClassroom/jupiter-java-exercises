e461ddc75d02faa2060b46922e1bba063ac3e53d
viviancan <vcnales@gmail.com>
2020-12-12 17:26:04 -0600

    added arrays exercises 1&2

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -8,5 +8,27 @@ public class ArraysExercises {
         System.out.println(Arrays.toString(numbers));
 
 
+        Person[] people = new Person[3];
+        people[0] = new Person("Vivian");
+        people[1] = new Person("Sophia");
+        people[2] = new Person("Justin");
+
+        for(Person person : people){
+            System.out.println(person.getName());
+        }
+
+        Person[] newArray = addPerson(people, new Person("Casey"));
+
+        for(Person person: newArray){
+            System.out.println(person.getName());
+        }
+
+    }
+
+    public static Person[] addPerson(Person[] pplArray, Person personToAdd){
+        Person[] tempArray = Arrays.copyOf(pplArray, pplArray.length+1);
+        tempArray[tempArray.length-1] = personToAdd;
+
+        return tempArray;
     }
 }
