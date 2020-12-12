9fd184df8a32ed2888610716f30d0bbbce2c7e59
viviancan <vcnales@gmail.com>
2020-12-12 17:15:56 -0600

    arrays exercises

---
+++ src/ArraysExercises.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers); // just a pointer to location in memory
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+}
